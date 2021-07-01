d44cda4625918e8dbefbd6c37b66e5c458a9f766
kinduff <kinduff@protonmail.com>
2021-07-01 23:34:02 +0200

    Adds missing comment for model

--- internal/model/player_summaries.go
+++ internal/model/player_summaries.go
@@ -1,5 +1,7 @@
 package model
 
+// PlayerSummaries stores information about the player's Steam profile,
+// it is used to convert a SteamID to a SteamName. Inverso of ResolveVanityUrl
 type PlayerSummaries struct {
 	Response struct {
 		Players []struct {
